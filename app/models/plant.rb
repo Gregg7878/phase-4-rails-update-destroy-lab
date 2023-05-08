41b9e72531be235fd9e321cf8a03f3ef5d966b6a
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-05-08 12:18:04 +0300

    modified the plant.rb modified routes.rb finished the lab

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,23 @@
-class Plant < ApplicationRecord
-end
+class PlantsController < ApplicationController
+    # ...
+  
+    def update
+      plant = Plant.find(params[:id])
+      plant.update(plant_params)
+      render json: plant
+    end
+  
+    # ...
+  
+    private
+  
+    def plant_params
+      params.require(:plant).permit(:name, :image, :price, :is_in_stock)
+    end
+    def destroy
+        plant = Plant.find(params[:id])
+        plant.destroy
+        head :no_content
+      end
+  end
+  
