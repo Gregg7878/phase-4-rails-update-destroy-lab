9280de5c884b3266c787abb823143337c8d412e5
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-08-13 12:48:38 +0300

    modified plants_controller.rb

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,29 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = Plant.find(params[:id])
+    if plant.update(plant_params)
+      render json: plant
+    else
+      render json: plant.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    plant = Plant.find(params[:id])
+    plant.destroy
+    head :no_content
+  end
+
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def plant_params
+    params.require(:plant).permit(:name, :image, :price, :is_in_stock)
   end
 end
